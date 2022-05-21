7fb712f0f061d1183500a8eb6dfa508ccbc85fac
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-05-21 09:10:28 +0300

    Designed the LogIn Page

--- app/src/main/java/com/ayesigyederrick/bbc/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ayesigyederrick/bbc/ui/home/HomeFragment.kt
@@ -23,7 +23,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
