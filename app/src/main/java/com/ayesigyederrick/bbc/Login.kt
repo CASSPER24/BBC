0daace25595695104858c9f95511ef1f59ff7814
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-05-21 09:47:39 +0300

    Designed Register page, Added OnclickListener to text 'LogIn' on Register page and 'Register' on Login page

--- app/src/main/java/com/ayesigyederrick/bbc/Login.kt
+++ app/src/main/java/com/ayesigyederrick/bbc/Login.kt
@@ -1,7 +1,9 @@
 package com.ayesigyederrick.bbc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,13 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         title = "LogIn"
+
+        //Set onclick to move to register page
+        val go_to_register = findViewById<TextView>(R.id.text_gotoregister)
+        go_to_register.setOnClickListener {
+            val i = Intent(this, Register::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/ayesigyederrick/bbc/Register.kt
+++ app/src/main/java/com/ayesigyederrick/bbc/Register.kt
@@ -1,11 +1,21 @@
 package com.ayesigyederrick.bbc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        title = "Register"
+
+        // set on click to move to login page
+        val go_to_login = findViewById<TextView>(R.id.text_gotologin)
+        go_to_login.setOnClickListener {
+            val intent_2 = Intent(this, Login::class.java)
+            startActivity(intent_2)
+        }
     }
 }
