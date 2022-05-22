12baa2813c712942616a9b769c40654b507cc63f
AYESIGYE DERRICK <ayesigyederrick@gmail.com>
2022-05-22 11:00:09 +0300

    Added Hander method in splashscreenn.kt to delay the splash screen

--- app/src/main/java/com/ayesigyederrick/bbc/SPLASHSCREEN.kt
+++ app/src/main/java/com/ayesigyederrick/bbc/SPLASHSCREEN.kt
@@ -1,11 +1,21 @@
 package com.ayesigyederrick.bbc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SPLASHSCREEN : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        //Handler method to delay splash screen count
+        Handler().postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
     }
 }
